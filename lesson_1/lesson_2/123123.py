c39a57c88713c7c9f6ffb5e796859d0a7782bff6
soulparadise <soulparadiss@gmail.com>
2020-12-25 04:51:04 +0300

    lesson 2

---
+++ lesson_1/lesson_2/123123.py
@@ -0,0 +1,7 @@
+for good in goods:
+    for feature_key, feature_value in good[1].items():
+        if feature_key in analitics:
+            analitics[feature_key].append(feature_value)
+        else:
+         analitics[feature_key] = [feature_value]
+print(analitics)
---
+++ lesson_1/lesson_2/task_5.py
@@ -0,0 +1,7 @@
+ratio_list = [7, 5, 3, 3, 2]
+print(ratio_list)
+add_ratio = int(input('Введите рейтинг для добавления'))
+ratio_list.append(add_ratio)
+ratio_list.sort()
+ratio_list.reverse()
+print(ratio_list)
